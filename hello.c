b1267d3ac180f6ec163bdb2e5d2cb023034cbc0c
lbenne <lexankb@rocketmail.com>
2020-08-20 11:01:55 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Lexan Benne
+ *         lbenne2@unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Lexan Benne\n");
+  printf("Undecided\n");
+
+  return 0;
+}
